1e3dd274d2b06e348cc94c86638174008cd28b51
hoangtran <htran@aquaveo.com>
2018-10-23 14:50:05 -0600

    Finish up test for tethys dask scheduler plugin

--- tests/test_tethys_dask_scheduler.py
+++ tests/test_tethys_dask_scheduler.py
@@ -25,20 +25,22 @@ class SchedulerPluginTest(unittest.TestCase):
         # Check result
         self.assertEqual('http://localhost:8080', tds_plugin.endpoint)
 
-    @mock.patch('tornado.util.Configurable')
-    @mock.patch('tornado.httpclient.AsyncHTTPClient')
-    def test_transition(self, mock_http_client, _):
-        # TODO: Need to check with Nathan
-        pass
-        # mock_scheduler = mock.MagicMock()
-        # tds_plugin = TethysSchedulerPlugin(endpoint='http://localhost:8080', scheduler=mock_scheduler)
-        # mock_scheduler.get_metadata.return_value = True
-        # mock_http_client.return_value = mock.MagicMock()
-        #
-        #
-        # # Call the method
-        # tds_plugin.transition(key='sum-4a062-d246-11e8-9aec-e33e562837e2', start='start', finish='complete')
-        #
-        # call_args = mock_http_client.call_args_list
-        #
-        # self.assertIs('', call_args)
+    @mock.patch('tethys_dask_scheduler.tethys_scheduler_plugin.AsyncHTTPClient')
+    def test_transition(self, mock_async):
+        endpoint = 'http://localhost:8080'
+        mock_scheduler = mock.MagicMock()
+        tds_plugin = TethysSchedulerPlugin(endpoint=endpoint, scheduler=mock_scheduler)
+        mock_scheduler.get_metadata.return_value = True
+        mock_http_client = mock.MagicMock()
+        mock_async.return_value = mock_http_client
+
+        job_key = 'sum-4a062-d246-11e8-9aec-e33e562837e2'
+        start = 'start'
+        finish = 'complete'
+
+        # Call the method
+        tds_plugin.transition(key=job_key, start=start, finish=finish)
+
+        rts_call_args = mock_http_client.fetch.call_args_list
+        check_str = '{}/update-dask-job-status/{}/?status={}-{}'.format(endpoint, job_key, start, finish)
+        self.assertEqual(check_str, rts_call_args[0][0][0])
